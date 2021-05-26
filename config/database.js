58a5cf0f178938574106afeb0440d3ee5c6cbaa5
khanhphamqn <khanhphamqn@gmail.com>
2021-05-26 15:21:14 +0700

    update code

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.ol0mu.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-mongo'),
+        username: env('DATABASE_USERNAME', 'pqkhanh'),
+        password: env('DATABASE_PASSWORD', 'trymybest15689'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
